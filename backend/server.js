022eadc49c9a84695180fec97b56f8399af68b06
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-08-30 17:57:20 +0900

    mySQL 연결하기1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const dbPool = require('./dbConfig'); // mySQL 연결이 필요한 파일에서 호출
 
 app.listen(8080, function() {
-    console.log('8080포트입니다?')
+    console.log('listening on 8080')
 })
