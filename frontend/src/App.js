ee851bb3895452dd8c5c8f62d7d11ccc25751c12
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-08-30 18:11:51 +0900

    install react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
+
+
   return (
     <div className="App">
-      
+      <Button variant="danger">123</Button>
     </div>
   );
 }
