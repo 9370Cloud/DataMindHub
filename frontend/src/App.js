461e69eb10de1ee7d6ddaae9815096bbbe049bd6
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-08-30 22:16:25 +0900

    가장 기본적인 메인페이지 html 만들었음

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Slogan from './components/Slogan';
 import TechStacks from './components/TechStacks';
 import Cards from './components/Cards';
+import Body4 from './components/Body4';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Slogan/>
       <TechStacks/>
       <Cards/>
+      <Body4/>
+      <Footer/>
     </div>
   );
 }
