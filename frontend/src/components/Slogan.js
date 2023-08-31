64d9a74a635c6b6adf44d9c24498e6d034550267
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-08-31 13:11:26 +0900

    react-typewriter

--- frontend/src/components/Slogan.js
+++ frontend/src/components/Slogan.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
+import Typewriter from "react-typewriter";
 
 function Slogan() {
   return (
@@ -7,8 +8,15 @@ function Slogan() {
       className="d-flex justify-content-center"
       style={{ height: "400px" }}
     >
-      <h1 style={{ margin: "auto" }}>
-        "흑백 배경에 큰따옴표, 대충 아무 글귀나 쓰면 명언같다." (바디1)
+      <h1 style={{ margin: "auto" }} id="slogan">
+        <Typewriter
+          delay={50}
+          typing={0.5}
+          minDelay={25}
+          maxDelay={100}
+        >
+          "흑백 배경에 큰따옴표, 대충 아무 글귀나 쓰면 명언같다."
+        </Typewriter>
       </h1>
     </Container>
   );
--- frontend/src/components/TechStacks.js
+++ frontend/src/components/TechStacks.js
@@ -2,8 +2,6 @@ import React from "react";
 import {
   AWSIcon,
   mySQLIcon,
-  typescriptIcon,
-  html5Icon,
   reactIcon,
   bootstrap5Icon,
   nodejsIcon,
@@ -16,25 +14,34 @@ import {
 import { Container } from "react-bootstrap";
 
 function TechStacks() {
+  const iconGrid = [
+    [javascriptIcon, mySQLIcon, nodejsIcon, reactIcon, AWSIcon],
+    [linuxIcon, gitIcon, dockerIcon, googlecloudIcon, bootstrap5Icon],
+  ];
+
+  const iconStyle = {
+    width: "150px", // Adjust the width as needed
+    height: "150px", // Adjust the height as needed
+  };
+
   return (
-    <Container style={{backgroundColor:'coral'}}>
+    <Container style={{  backgroundColor:"#FFFBE9", width:"100%"  }}>
       <Container
         className="d-flex justify-content-center align-items-center"
         style={{ height: "100px" }}
       >
-        <h2>Tech Stacks(바디2)</h2>
+        <h2 style={{color:"#374E34"}}>Tech Stacks</h2>
       </Container>
-      <Container className="d-flex justify-content-center align-items-center">
-        {javascriptIcon}
-        {mySQLIcon}
-        {nodejsIcon}
-        {reactIcon}
-        {AWSIcon}
-        {linuxIcon}
-        {gitIcon}
-        {dockerIcon}
-        {googlecloudIcon}
-        {bootstrap5Icon}
+      <Container className="d-flex flex-column align-items-center">
+        {iconGrid.map((row, rowIndex) => (
+          <div key={rowIndex} className="d-flex">
+            {row.map((icon, columnIndex) => (
+              <div key={columnIndex} style={{ margin: "10px" }}>
+                {React.cloneElement(icon, { style: iconStyle })}
+              </div>
+            ))}
+          </div>
+        ))}
       </Container>
     </Container>
   );
