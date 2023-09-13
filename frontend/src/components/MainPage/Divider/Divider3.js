ca6ee5b1632300db810e5ef3094b3ba14bc82546
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-09-13 13:05:44 +0900

    cards 2

--- frontend/src/components/MainPage/Divider/Divider3.js
+++ frontend/src/components/MainPage/Divider/Divider3.js
@@ -1,22 +1,23 @@
-import './Divider3.css'
+import "./Divider3.css";
 
-function Divider3(){
-
-    return(<div style={{width:'100%', backgroundColor:'red'}}>
-    <div class="custom-shape-divider-top-1694490609">
-      <svg
-        data-name="Layer 1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1200 120"
-        preserveAspectRatio="none"
-      >
-        <path
-          d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
-          class="shape-fill"
-        ></path>
-      </svg>
+function Divider3() {
+  return (
+    <div style={{ width: "100%", backgroundColor: "red" }}>
+      <div class="custom-shape-divider-top-1694490609">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
     </div>
-    </div>)
+  );
 }
 
-export default Divider3
+export default Divider3;
