829c06ac60205aeb1ec5b2a35e0679dadd8e4cf2
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-09-21 17:54:40 +0900

    frontend

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -8,7 +8,7 @@ function NavigationBar() {
     <div className="NavigationBar">
       <Navbar expand="lg" className="bg-body-white">
         <Container>
-          <Navbar.Brand href="#home" style={{fontWeight:'bold'}}>dataMindHub</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{fontWeight:'bold'}}>dataMindHub💫</Navbar.Brand>
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
             style={{ marginBottom: "10px" }}
